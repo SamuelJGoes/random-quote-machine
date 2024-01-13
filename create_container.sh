e4b520e8361d73c04ad20bc47116ec799b2e7c18
Samuel Goes <samuel.goes@automni.com.br>
2024-01-12 21:11:00 -0300

    Configure eslint with prettier

--- create_container.sh
+++ create_container.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 CONTAINER_NAME='random_quote_app'
-IMAGE_NAME='automni/automni_config_web:v0.0.0'
+IMAGE_NAME='samjgoes/random_quote_machine:v0.0.0'
 
 docker create \
 	--name $CONTAINER_NAME \
-    --net=host\
-    -v /$HOME/random_quote_machine/src:/automni_config_web/src \
-    -v /$HOME/random_quote_machine/public:/automni_config_web/public \
+    -p 3000:3000 \
+    -v /$HOME/random-quote-machine/src:/app/src \
+    -v /$HOME/random-quote-machine/public:/app/public \
+    -v /$HOME/random-quote-machine/package.json:/app/package.json \
+    -v /$HOME/random-quote-machine/.eslintrc.json:/app/.eslintrc.json \
+    -v /$HOME/random-quote-machine/.prettierrc:/app/.prettierrc \
     $IMAGE_NAME 
